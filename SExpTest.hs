17bda42b9e56c8286330e64ec4bcd9278ec05fdb
HIBINO Kei <hibi@utmc.or.jp>
2011-07-07 20:58:26 +0900

    Add type signature.

--- SExpTest.hs
+++ SExpTest.hs
@@ -1,6 +1,7 @@
 module SExpTest (
   t1,
-  test0, test1, test2
+  test0, test1, test2,
+  str3, test3
   ) where
 
 import Language.LazyS.ParseResult (ParseResult)
@@ -20,13 +21,15 @@ test2 =  parseExpr "(let ((x 1) (y . (x))) y)"
  
 t1, test0, test1, test2 :: ParseResult SExp
 
-str3 = unlines
-       ["(let (((tak x y z)",
-        "       (if (<= x y)",
-        "           y",
-        "           (tak (tak (- x 1) y z)",
-        "                (tak (- y 1) z x)",
-        "                (tak (- z 1) x y)))))",
-        "  (tak 10 5 1))"]
+str3 :: String
+str3 =  unlines
+        ["(let (((tak x y z)",
+         "       (if (<= x y)",
+         "           y",
+         "           (tak (tak (- x 1) y z)",
+         "                (tak (- y 1) z x)",
+         "                (tak (- z 1) x y)))))",
+         "  (tak 10 5 1))"]
 
-test3 = parseExpr str3
+test3 :: ParseResult SExp
+test3 =  parseExpr str3
